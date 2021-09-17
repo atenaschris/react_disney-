2734a798d3de24b05d7b01afd8a7269a4bef08a5
Christofer cardone <chriscardone69@gmail.com>
2021-09-17 18:22:00 +0200

    background  added to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,59 @@
 import styled from "styled-components";
 
-const Home = (props)=>{
-    return (
-        <div>Home</div>
-    )
+const Home = (props) => {
+  return (
+    <Container>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+    </Container>
+  );
 };
 
+const Container = styled.main`
+  color: white;
+  position: relative;
+  overflow-x: hidden;
+  min-height: calc(100vh - 250px);
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after{
+      content: '';
+    position: absolute;
+    background: url("/images/home-background.png")  center center / cover no-repeat fixed ;
+    inset:0px;
+    z-index: -1;
+  }
+`;
 export default Home;
