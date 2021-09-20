38a005f58f261b2a5048430df8b001fe1db085f1
Christofer cardone <chriscardone69@gmail.com>
2021-09-20 09:42:04 +0200

    New Disneey, Originals and Trending components added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney";
 import Recommends from "./Recommends";
 import Viewers from './Viewers';
+import Originals from "./Originals";
+import Trending from "./Trending";
 
 const Home = (props) => {
   return (
@@ -9,6 +12,9 @@ const Home = (props) => {
       <ImgSlider />
       <Viewers/>
       <Recommends/>
+      <NewDisney/>
+      <Originals/>
+      <Trending/>
     </Container>
   );
 };
---
+++ src/components/Originals.js
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+
+import { Link } from "react-router-dom";
+
+const Originals = () => {
+  return (
+    <Container>
+      <h4>Originals</h4>
+      <Content>
+        <Wrap>
+          <Link to="">
+            <img src="/images/vieWers-disney.png" alt="" />
+          </Link>
+        </Wrap>
+        <Wrap>
+          <Link to="">
+            <img src="/images/vieWers-disney.png" alt="" />
+          </Link>
+        </Wrap>
+        <Wrap>
+          <Link to="">
+            <img src="/images/vieWers-disney.png" alt="" />
+          </Link>
+        </Wrap>
+        <Wrap>
+          <Link to="">
+            <img src="/images/vieWers-disney.png" alt="" />
+          </Link>
+        </Wrap>
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 0 0 26px 0;
+`;
+
+const Content = styled.div`
+  display: grid;
+  gap: 25px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+`;
+
+const Wrap = styled.div`
+ padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  border: 3px solid rgba(249,249,249,0.1);
+
+  img{
+      position: absolute;
+      inset: 0px;
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      display: block;
+      opacity: 1;
+      z-index: 1;
+      transition: transform 250ms ease;
+      transform:scale(1);
+  }
+
+  &:hover{
+      box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+      transform:scale(1.05);
+      transform-origin: left center;
+      border-color: rgba(249,249,249,0.8);
+
+      img{
+        transform:scale(1.1);
+      }
+  }
+`;
+
+export default Originals;
